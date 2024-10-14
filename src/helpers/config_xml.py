fd817cea920843722512f58257e4b42b6cc62cbb
jaykold <christianichebi@gmail.com>
2024-10-14 06:29:22 +0100

    Updated config_xml

--- src/helpers/config_xml.py
+++ src/helpers/config_xml.py
@@ -1,14 +1,15 @@
-import xml.sax.saxutils as saxutils
+#import xml.sax.saxutils as saxutils
+from utils.escape_xml import escape_xml
 
 def config_xml(pipeline_script):
-    escaped_pipeline_script = saxutils.escape(pipeline_script)
+    escaped_pipeline_script = escape_xml(pipeline_script)
     return f'''<?xml version='1.1' encoding='UTF-8'?>
     <flow-definition plugin="workflow-job@2.40">
     <description></description>
     <keepDependencies>false</keepDependencies>
     <properties/>
     <definition class="org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition" plugin="workflow-cps@2.87">
-    <script>\n{escaped_pipeline_script}</script>
+    <script>{escaped_pipeline_script}</script>
     <sandbox>true</sandbox>
     </definition>
     <triggers/>
