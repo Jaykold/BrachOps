94646081d46d7557f8178199411a13880bd8e2af
jaykold <christianichebi@gmail.com>
2024-10-22 21:50:39 +0100

    update

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 import uvicorn
 from fastapi import FastAPI
-from src.routes import routes
+from src.routes.routes import router
 import config
 
 app = FastAPI(debug=config.DEBUG)
 
-routes.router(app)
+app.include_router(router)
 
 @app.get('/')
 async def health_check():
     return "OK"
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=config.PORT)
+    uvicorn.run("app:app", host="0.0.0.0", reload=True, port=config.PORT)
